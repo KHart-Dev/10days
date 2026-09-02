f0d0e570fcdc2fb64621ec024399b13dbad86c65
kase haruto <k023g0052@g.neec.ac.jp>
2026-09-03 07:52:52 +0900

    シーンにとげブロックを配置しました。また、衝突レイヤーに障害物ブロックを追加しました。

--- Game/Actor/Obstacle/Thorn.cpp
+++ Game/Actor/Obstacle/Thorn.cpp
@@ -1,5 +1,12 @@
 #include "Thorn.h"
 
+// game
+#include <Game/Collision/CollisionLayerUtil.h>
+
+// engine
+#include <Engine/Graphics/Camera/3d/Camera3d.h>
+#include <Engine/Graphics/Camera/Manager/CameraManager.h>
+
 Thorn::Thorn() :Actor("cone.obj", "thorn") {
 	//初期化時にスケールを小さくしておく
 	worldTransform_.scale *= 0.5f;
@@ -8,6 +15,12 @@ Thorn::~Thorn() = default;
 
 // 衝突時処理
 void Thorn::OnCollisionEnter(Collider* other){
+	if (!other) {
+		return;
+	}
+
+	const auto playerLayer = GameCollision::FindLayerId("Thon");
+		CameraManager::GetMain3d()->StartShake(1.0f, 0.1f);
 
 }
 

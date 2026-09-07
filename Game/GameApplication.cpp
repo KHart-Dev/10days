afe024857565572315775151705a248c4268aa03
kase haruto <k023g0052@g.neec.ac.jp>
2026-09-08 03:53:54 +0900

    windowのタイトルを修正

--- Game/GameApplication.cpp
+++ Game/GameApplication.cpp
@@ -1,9 +1,16 @@
 #include "GameApplication.h"
 
+// game
 #include <Game/Result/ResultCarry.h>
+
+// engine
+#include <CalyxEngine/Engine.h>
 
 void GameApplication::OnInitialize() {
 	ResultCarry::Clear();
+
+	Calyx::SetWindowTitle("4004_かけろ！にんげんブリッジ");
+
 }
 
 void GameApplication::OnUpdate() {}
--- Game/GameMain.cpp
+++ Game/GameMain.cpp
@@ -1,11 +1,12 @@
 #include <CalyxEngine/CalyxEngine.h>
+#include <CalyxEngine/Engine.h>
 
 #include <Generated/Foundation/Reflection/CalyxGameObjectRegistry.generated.h>
 #include "GameApplication.h"
 
 extern "C" __declspec(dllexport) Calyx::Application* CreateCalyxApplication() {
-	CalyxEngine::RegisterGeneratedGameSceneObjects();
 
+	CalyxEngine::RegisterGeneratedGameSceneObjects();
 	return new GameApplication();
 }
 

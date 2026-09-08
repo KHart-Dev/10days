441d714243ab851767a0a328f96e0638fa4b24d6
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-09-08 15:46:56 +0900

    リザルト音

--- Game/Meteorite/InResult/MeteoriteWarning.cpp
+++ Game/Meteorite/InResult/MeteoriteWarning.cpp
@@ -6,6 +6,7 @@
 
 // game
 #include "FallingMeteorite.h"
+#include <Game/Audio/GameAudio.h>
 
 // std
 #include <numbers>
@@ -91,7 +92,12 @@ void MeteoriteWarning::UpdateBlinking(float dt) {
 	timer_ += dt;
 
 	// 1周期の前半だけ見せる
-	SetDrawEnable(timer_ < settings_.blinkPeriod * 0.5f);
+	const bool nowVisible = timer_ < settings_.blinkPeriod * 0.5f;
+	SetDrawEnable(nowVisible);
+	if (nowVisible && !wasVisible_) {
+		GameAudio::PlaySe(GameAudio::kSeDanger);
+	}
+	wasVisible_ = nowVisible;
 
 	if (timer_ < settings_.blinkPeriod) {
 		return;
@@ -138,6 +144,7 @@ void MeteoriteWarning::DropMeteorite() {
 					settings_.impactHold);
 
 	meteorite_ = std::move(meteorite);
+	GameAudio::PlaySe(GameAudio::kSeMeteo);
 }
 
 void MeteoriteWarning::DisableGravity() {
